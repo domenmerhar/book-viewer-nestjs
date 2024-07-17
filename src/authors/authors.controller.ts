aa6bfebbac72832cf377ff998d7350afca7c1ae8
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 10:10:04 +0200

    Update auhtors.controller.ts

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -8,6 +8,8 @@ import {
   Post,
 } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
+import { AuthorDto } from './dto/author.dto';
+import { Author } from './author.entity';
 
 @Controller('authors')
 export class AuthorsController {
@@ -24,13 +26,16 @@ export class AuthorsController {
   }
 
   @Post()
-  addAuthor(@Body('name') name: string): string {
-    return name;
+  addAuthor(@Body() authorDto: AuthorDto): Promise<Author> {
+    return this.authorsService.createAuthor(authorDto);
   }
 
-  @Patch('/:id')
-  updateAuthor(@Param('id') id: string): string {
-    return id;
+  @Patch('/:id/name')
+  updateAuthor(
+    @Param('name') name,
+    @Param('id') id: string,
+  ): Promise<AuthorDto> {
+    return this.authorsService.updateAuthorName(name, id);
   }
 
   @Delete('/:id')
