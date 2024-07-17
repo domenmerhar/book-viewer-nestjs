fe94acbeee6448d0e2f03a67e199d244f0b5c759
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 19:58:35 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -21,8 +18,7 @@ import { BooksModule } from './books/books.module';
     }),
     AuthorsModule,
     BooksModule,
+    AuthModule,
   ],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
 })
 export class AppModule {}
