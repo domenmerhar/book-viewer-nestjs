911263170501578e0f3073d10fd50325f829450b
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 08:35:01 +0200

    Add books.controller.ts

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  //TODO add filters
+  @Get()
+  getAllBooks(): string {
+    return 'All books';
+  }
+
+  @Get('/:id')
+  getBookById(@Param('id') id: string): string {
+    return id;
+  }
+
+  @Post()
+  addBook(@Body('name') name: string): string {
+    return name;
+  }
+
+  @Patch('/:id')
+  updateBook(@Param('id') id: string): string {
+    return id;
+  }
+
+  @Delete('/:id')
+  deleteBook(@Param('id') id: string): string {
+    return id;
+  }
+}
