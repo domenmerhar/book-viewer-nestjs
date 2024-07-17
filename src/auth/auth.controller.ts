e4a118326b87f6d93a93a391fd2e0058105c819e
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 19:59:08 +0200

    Update auth.controller, service.ts; signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserDto } from './dto/user.dto';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signup(@Body() userDto: UserDto) {
-    return userDto;
+  signup(@Body() userDto: CreateUserDto): Promise<User> {
+    return this.authService.signup(userDto);
   }
 
   @Post('/signin')
-  singin(@Body() userDto: UserDto) {
+  singin(@Body() userDto: CreateUserDto) {
     return userDto;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  signup(createUserDto: CreateUserDto): Promise<User> {
+    return this.usersRepository.signup(createUserDto);
+  }
+}
