8c26bec9b2bc6c34c6f09235d1d530d757694d0e
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 18:15:20 +0200

    Update books.module.ts

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -4,10 +4,11 @@ import { BooksService } from './books.service';
 import { Book } from './book.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoooksRepository } from './books.repository';
+import { AuthorsHelperRepository } from './authors-helper.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
   controllers: [BooksController],
-  providers: [BooksService, BoooksRepository],
+  providers: [BooksService, BoooksRepository, AuthorsHelperRepository],
 })
 export class BooksModule {}
