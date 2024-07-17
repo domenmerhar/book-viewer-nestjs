6efcb9a8ecb1ea9a50faffea319869bf7a3ba9e2
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 08:47:06 +0200

    Add auth.controller.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
+import { AuthorsController } from './authors/authors.controller';
+import { AuthorsService } from './authors/authors.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, BooksController],
-  providers: [AppService, BooksService],
+  controllers: [AppController, BooksController, AuthorsController, AuthController],
+  providers: [AppService, BooksService, AuthorsService, AuthService],
 })
 export class AppModule {}
