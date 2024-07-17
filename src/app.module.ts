943f7cd433bc6219c670439acac3ea7c04e91bb5
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 09:21:24 +0200

    Add TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,27 @@ import { AuthorsController } from './authors/authors.controller';
 import { AuthorsService } from './authors/authors.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController, BooksController, AuthorsController, AuthController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'book-viewer',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [
+    AppController,
+    BooksController,
+    AuthorsController,
+    AuthController,
+  ],
   providers: [AppService, BooksService, AuthorsService, AuthService],
 })
 export class AppModule {}
