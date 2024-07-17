8f32299e2f9711932d1ba59721311ab428be6b08
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-17 10:09:56 +0200

    Update authors.service.ts

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { AuthorDto } from './dto/author.dto';
+import { AuthorsRepository } from './authors.repository';
+import { Author } from './author.entity';
 
 @Injectable()
-export class AuthorsService {}
+export class AuthorsService {
+  constructor(private authorsRepository: AuthorsRepository) {}
+
+  createAuthor(authorDto: AuthorDto): Promise<Author> {
+    return this.authorsRepository.createAuthor(authorDto);
+  }
+
+  updateAuthorName(name: string, id: string): Promise<Author> {
+    return this.authorsRepository.updateAuthorName(name, id);
+  }
+}
