f7dfcf8a4f74d6c5b6140e4a62e11627a4967042
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-18 09:05:43 +0200

    Update add-book.dto.ts; Validation

--- src/books/dto/add-book.dto.ts
+++ src/books/dto/add-book.dto.ts
@@ -1,8 +1,32 @@
+import {
+  IsBooleanString,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class AddBookDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   subjects: string;
+
+  @IsOptional()
+  @IsString()
   author: string;
+
+  @IsString()
+  @IsNotEmpty()
   languages: string;
+
+  @IsBooleanString()
+  @IsNotEmpty()
   copyright: boolean;
+
+  @IsNumberString()
   year: number;
 }
