e8c2ec42ebae291c16e1d711c3b1192e3660f87f
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-18 09:22:55 +0200

    Update author.dto.ts; Validation

--- src/authors/dto/author.dto.ts
+++ src/authors/dto/author.dto.ts
@@ -1,5 +1,22 @@
+import {
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class AuthorDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNumberString()
+  @IsOptional()
+  @IsNotEmpty()
   birthYear: number;
+
+  @IsNumberString()
+  @IsOptional()
+  @IsNotEmpty()
   deathYear: number;
 }
