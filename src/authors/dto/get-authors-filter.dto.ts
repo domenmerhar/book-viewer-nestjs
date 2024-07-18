e19fe22d5b0be5b2ada84c3a08d7a8be20dca818
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-18 09:18:24 +0200

    Update get-authors-filter.dto.ts; Implement validation

--- src/authors/dto/get-authors-filter.dto.ts
+++ src/authors/dto/get-authors-filter.dto.ts
@@ -1,5 +1,21 @@
+import {
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  Validate,
+} from 'class-validator';
+import { SortEnum } from '../sort.enum';
+import { IsSortString } from '../validators/is-sort-string';
+
 export class GetAuthorsFilter {
+  @IsNotEmpty()
+  @IsOptional()
   search: string;
+
+  @IsNumberString()
+  @IsOptional()
   year: number;
-  sort: 'asc' | 'desc';
+
+  @Validate(IsSortString)
+  sort: SortEnum;
 }
