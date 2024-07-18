6ac3084dee0e8ee3a835ca2813557ac6966f16bf
Domen Merhar <domen.merhar@students.vegova.si>
2024-07-18 09:22:39 +0200

    Update create-user.dto.ts; Implement validation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
   password: string;
 }
